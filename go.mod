52bb20a7e1793f7008c608edfce7568974cd098d
Your Name <you@example.com>
2021-03-18 18:18:59 +0700

    main APP02

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/RizkyAssidiqi/APP_02
+
+go 1.15
+
+require github.com/RizkyAssidiqi/golang02/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	P02 "github.com/RizkyAssidiqi/golang02/v2"
+)
+
+func main() {
+	fmt.Println(P02.NamaSaya("Rizky"))
+}
